4991551051baf94bba3b986eda065704b3a3b4c5
Shimadakunn <lcombaret@hotmail.co.jp>
2024-01-20 22:55:30 +0100

    Code for Submission

--- app/home/send.tsx
+++ app/home/send.tsx
@@ -76,58 +76,44 @@ const Send: React.FC<SendProps> = ({ open, setOpen, address }) => {
   }, [address]);
 
   async function Send() {
-    if (
-      amount &&
-      balance &&
-      setBalance &&
-      setTransactions &&
-      Number(
-        (
-          amount *
-          cryptos.find((crypto) => crypto.value === selectedCrypto)!.price
-        ).toFixed(4)
-      ) <= balance
-    ) {
-      setIsLoading(true);
-      setTimeout(() => {
-        setBalance!(
-          balance -
-            Number(
-              (
-                amount *
-                cryptos.find((crypto) => crypto.value === selectedCrypto)!.price
-              ).toFixed(2)
-            )
-        );
-        setTransactions!((prevTransactions) => [
-          [
-            receiver,
-            receiver,
-            Number(
-              (
-                amount *
-                cryptos.find((crypto) => crypto.value === selectedCrypto)!.price
-              ).toFixed(2)
-            ),
-          ],
-          ...prevTransactions,
-        ]);
-        setReceiver("");
-        setAmount(0);
-        toast({
-          title: "Funds sent",
-        });
-        setOpen(false);
-        setIsLoading(false);
-      }, 1000);
-    } else {
+    sendTransaction({to: receiver, value: parseEther(amount!.toString())});
+    if (!isLoading && amount && balance && status === "success") {
+      setBalance!(
+        balance -
+          Number(
+            (
+              amount *
+              cryptos.find((crypto) => crypto.value === selectedCrypto)!.price
+            ).toFixed(2)
+          )
+      );
+      setTransactions!((prevTransactions) => [
+        [
+          receiver,
+          receiver,
+          Number(
+            (
+              amount *
+              cryptos.find((crypto) => crypto.value === selectedCrypto)!.price
+            ).toFixed(2)
+          ),
+        ],
+        ...prevTransactions,
+      ]);
+      setReceiver("");
+      setAmount(0);
+      toast({
+        title: "Funds sent",
+      });
+      setOpen(false);
+    } else if (!isLoading && status === "error") {
       toast({
         variant: "destructive",
         title: "Transaction failed",
         description: "Please check your balance and your receiver address",
       });
     }
-  };
+  }
 
   return (
     <Drawer open={open} onOpenChange={setOpen}>
--- app/receive/page.tsx
+++ app/receive/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 import {QRCodeSVG} from 'qrcode.react';
+import { useAccount } from "wagmi";
 
   export default function Home() {
+    const { address } = useAccount();
     return (
       <main className="h-[84vh] flex items-center justify-center">
-        <QRCodeSVG value="http://192.168.1.18:3000/home?address=0x63972913FC6FA0dd24250E0F0cEe8724b47a8e14" size={250}/>
+        <QRCodeSVG value={`https://legho.vercel.app/home?address=${address}`} size={250}/>
       </main>
     )
   }
